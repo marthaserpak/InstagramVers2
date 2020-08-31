c018f69acfbdf96c2bb247bae2b4fb033ada6c9b
marthaserpak <m.v.serpak@gmail.com>
2020-08-31 18:41:24 +0300

    d

--- app/src/main/java/com/marthaserpak/instagramvers2/HomeActivity.kt
+++ app/src/main/java/com/marthaserpak/instagramvers2/HomeActivity.kt
@@ -1,11 +1,13 @@
 package com.marthaserpak.instagramvers2
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : BaseActivity(0) {
     private val TAG = "HomeActivity"
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,14 +15,27 @@ class HomeActivity : BaseActivity(0) {
         Log.d(TAG, "onCreate")
         bottomNavigation()
 
-        val auth = FirebaseAuth.getInstance()
-        auth.signInWithEmailAndPassword("m.v.serpak@gmail.com", "password")
+        mAuth = FirebaseAuth.getInstance()
+        mAuth.signOut()
+
+        /*auth.signInWithEmailAndPassword("m.v.serpak@gmail.com", "password")
             .addOnCompleteListener{
                 if (it.isSuccessful) {
                     Log.i(TAG,  "auth: successful")
                 } else {
                     Log.i(TAG, "auth: failure", it.exception)
                 }
-            }
+            }*/
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (mAuth.currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+
+            /**если пользователь не зашел в акк, чтоб он не смог при нажатии
+            кнопки "назад" вернуться в HomeActivity */
+            finish()
+        }
     }
 }
---
+++ app/src/main/java/com/marthaserpak/instagramvers2/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.marthaserpak.instagramvers2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class LoginActivity : AppCompatActivity() {
+    private val TAG = "LoginActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        Log.d(TAG, "onCreate")
+    }
+}
